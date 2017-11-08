b8a3ba6976f8d9d2c8899080400d0019f872cad3
Adamos Solomou <adamossolomou@dyn1183-104.wlan.ic.ac.uk>
2017-11-08 12:34:23 +0000

    Updated create_n512.sh script to accommodate operation in a fresh environment

--- scripts/create_n512.sh
+++ scripts/create_n512.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+#Added in order to accommodate operation in a fresh environment (e.g AWS)
 mkdir bin 
 make tools
 make 
+
 for i in $(seq -w 0 31); do
     mkdir -p w
     bin/generate_sparse_layer 512 512 > w/n512_${i}.bin;
 done
-
-#script genereates 32 layers, each of are 512 wide (512 nodes in each layer)
